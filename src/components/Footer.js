3c3fc08d4d66e6b11137fb4e501363ae65a06ba8
drakata27 <theall3xander@gmail.com>
2024-09-02 20:14:58 +0100

    Added web icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 import GitHub from '../assets/github.png'
 import LinkedIn from '../assets/linkedin.png'
 import Mail from '../assets/mail.png'
+import Web from '../assets/web.png'
 
 import {Link} from 'react-router-dom'
 
@@ -16,6 +17,7 @@ const Footer = () => {
           <Link to='https://www.linkedin.com/in/aleksandar-drakaliyski/' target="_blank"><img className='social-icon' src={LinkedIn} alt='linkedin'/></Link>
           <Link to='https://github.com/drakata27' target="_blank"><img className='social-icon' src={GitHub} alt='github'/></Link>
           <Link to='mailto:aleks.draka02@gmail.com' target="_blank"><img className='social-icon' src={Mail} alt='mail'/></Link>
+          <Link to='https://www.aleksdraka.online/' target="_blank"><img className='social-icon' src={Web} alt='website'/></Link>
         </div>
       </div>
     </div>
